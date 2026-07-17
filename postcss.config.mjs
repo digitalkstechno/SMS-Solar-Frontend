98af41c6c72744a11f199f6528efb208a4e9225c
manavdigitalks <manavdigitalks@gmail.com>
2026-07-17 14:25:57 +0530

    isue fix

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,18 @@
 const config = {
   plugins: {
     "@tailwindcss/postcss": {},
+    "postcss-preset-env": {
+      features: {
+        'nesting-rules': true
+      },
+      browsers: [
+        "> 0.2%",
+        "last 2 versions",
+        "Chrome >= 109",
+        "not dead"
+      ]
+    },
+    "autoprefixer": {},
   },
 };
 
